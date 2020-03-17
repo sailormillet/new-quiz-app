de56b5736dfdc4f76aa70125779764fb71cab9ae
Amy <sailor_mi@163.com>
2020-03-17 18:12:51 +0800

    deploy config:rocket:

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const isProd = process.env.NODE_ENV === 'production';
 module.exports = withLess({
   distDir: 'dist',
   pageExtensions: ['jsx', 'js'],
-  assetPrefix: isProd ? 'https://cdn.xxx.com' : '',
+  // assetPrefix: isProd ? 'https://cdn.xxx.com' : '',
   // useFileSystemPublicRoutes: false
   /* config options here */
   webpack(config, options) {
--- services/index.js
+++ services/index.js
@@ -1,7 +1,10 @@
 import fetch from 'isomorphic-unfetch';
+
 const dev = process.env.NODE_ENV !== 'production';
 
-export const server = dev ? 'http://localhost:3000' : 'https://xxx.server.com';
+export const server = dev
+  ? 'http://localhost:3000'
+  : 'https://api.binance.vision';
 
 export const getGlossaries = () => {
   return fetch(`${server}/api/glossaries`);
