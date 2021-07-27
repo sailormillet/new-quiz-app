33d457287eb8a1eb4e7dd8f97cfb602a4410a760
cxm <chenxiaomin1-jk@360jinrong.net>
2021-07-27 21:06:30 +0800

    upgrade

--- postcss.config.js
+++ postcss.config.js
@@ -7,23 +7,24 @@ module.exports = {
       },
       stage: 3
     }),
-    require('postcss-pxtorem')({
-      rootValue: 16,
-      unitPrecision: 5,
-      mediaQuery: false,
-      minPixelValue: 0,
-      propList: [
-        '*background*',
-        '*padding*',
-        '*margin*',
-        'letter-spacing',
-        '*width',
-        'left',
-        'font*',
-        'right',
-        'top',
-        'bottom'
-      ]
-    })
+//     require('postcss-pxtorem')({
+//       rootValue: 16,
+//       unitPrecision: 5,
+//       mediaQuery: false,
+//       minPixelValue: 0,
+//       propList: [
+//         '*background*',
+//         '*padding*',
+//         '*margin*',
+//         'letter-spacing',
+//         '*width',
+//         'left',
+//         'font*',
+//         'right',
+//         'top',
+//         'bottom'
+//       ]
+//     })
+
   ]
 };
